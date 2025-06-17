bc6743f415d086b80d0753f69c2126b4efd84464
Jessecds09 <jesse.cidra.santos@escola.pr.gov.br>
2025-06-17 15:08:30 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,52 @@ criaCartao(
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 );
+
+// Novas perguntas abaixo
+criaCartao(
+    'Matemática',
+    'Quanto é 9 x 7?',
+    '9 x 7 = 63'
+);
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Deodoro da Fonseca'
+);
+
+criaCartao(
+    'Ciências',
+    'Qual é o maior planeta do Sistema Solar?',
+    'Júpiter é o maior planeta do Sistema Solar.'
+);
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu Dom Casmurro?',
+    'Machado de Assis'
+);
+
+criaCartao(
+    'Geografia',
+    'Em que continente fica o Egito?',
+    'O Egito fica no continente africano.'
+);
+
+criaCartao(
+    'Programação',
+    'O que significa HTML?',
+    'HTML significa HyperText Markup Language'
+);
+
+criaCartao(
+    'Artes',
+    'Quem pintou a Mona Lisa?',
+    'Leonardo da Vinci'
+);
+
+criaCartao(
+    'Esportes',
+    'Quantos jogadores tem um time de futebol?',
+    'Um time de futebol tem 11 jogadores em campo.'
+);
