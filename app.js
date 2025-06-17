f3ddd58795c880fd4c9de9d2b1d8aa76197ed5fa
Jessecds09 <jesse.cidra.santos@escola.pr.gov.br>
2025-06-17 14:47:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+// Função para criar um cartão de flashcard
+function criaCartao(categoria, pergunta, resposta) {
+    const container = document.getElementById('container');
+    const cartao = document.createElement('article');
+    cartao.className = 'cartao';
 
+    cartao.innerHTML = `
+        <div class="cartao__conteudo">
+            <h3>${categoria}</h3>
+            <div class="cartao__conteudo__pergunta">
+                <p>${pergunta}</p>
+            </div>
+            <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+            </div>
+        </div>
+    `;
+
+    // Alterna a exibição da resposta
+    cartao.addEventListener('click', () => {
+        cartao.classList.toggle('mostrar-resposta');
+    });
+
+    container.appendChild(cartao);
+}
